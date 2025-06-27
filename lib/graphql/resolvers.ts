23f5b6e3010943b5ed8febded344323adc7986ca
John Timilsina <johnt@silvertrak.com.au>
2025-06-27 14:38:38 +1000

    feat: set up front end and back end with basic todo working

---
+++ lib/graphql/resolvers.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export const resolvers = {
+  Query: {
+    todos: () => prisma.todo.findMany({ orderBy: { createdAt: "desc" } }),
+  },
+  Mutation: {
+    addTodo: (_: any, { text }: { text: string }) =>
+      prisma.todo.create({ data: { text } }),
+    deleteTodo: async (_: any, { id }: { id: number }) => {
+      await prisma.todo.delete({ where: { id } })
+      return true
+    },
+    toggleTodo: async (_: any, { id }: { id: number }) => {
+      const todo = await prisma.todo.findUnique({ where: { id } })
+      return prisma.todo.update({
+        where: { id },
+        data: { completed: !todo?.completed },
+      })
+    },
+  },
+}
---
+++ lib/graphql/schema.ts
@@ -0,0 +1,20 @@
+import { gql } from "graphql-tag"
+
+export const typeDefs = gql`
+  type Todo {
+    id: Int!
+    text: String!
+    completed: Boolean!
+    createdAt: String!
+  }
+
+  type Query {
+    todos: [Todo!]!
+  }
+
+  type Mutation {
+    addTodo(text: String!): Todo!
+    deleteTodo(id: Int!): Boolean!
+    toggleTodo(id: Int!): Todo!
+  }
+`
